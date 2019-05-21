c072aba361dba521c871b82f7605ee0a2c35c03b
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-05-21 11:20:28 +0200

    quête scss

---
+++ precious.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="bootstrap.css" rel="stylesheet" media="all" type="text/css">
+    <link href="style.css" rel="stylesheet" media="all" type="text/css">
+    <title>My Precious</title>
+    <link href="https://fonts.googleapis.com/css?family=Merienda+One|Permanent+Marker|Roboto|Source+Sans+Pro" rel="stylesheet">
+</head>
+<body>
+    <?php require 'header.html' ?>
+    <h1>SCSS</h1>
+    <main class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="ring-one"></div>
+            <h2 class="text-center p-3">My precious</h2>
+            <div class="ring-one"></div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="ring-two"></div>
+            <h2 class="text-center d-flex align-self-center p-3">The one ring</h2>
+        </div>
+        <div>
+            <div class="rings row justify-content-center">
+                <div class="d-flex align-self-center ring-three"></div>
+                <div class="d-flex align-self-center ring-three"></div>
+                <div class="d-flex align-self-center ring-three"></div>
+                <div class="d-flex align-self-center ring-three"></div>
+                <div class="d-flex align-self-center ring-three"></div>
+                <div class="d-flex align-self-center ring-three"></div>
+            </div>
+            <h2 class="text-center p-3">Please find it!</h2>
+        </div>
+    </main>
+    <?php require 'footer.html' ?>
+</body>
+
+</html>
